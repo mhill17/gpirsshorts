1632afda2e1afe21e9efb22df8444a5874c0dcf6
Hill <mhill17@jaguarlandrover.com>
2025-11-11 09:40:06 +0000

    Initial commit

---
+++ hook-streamlit.py
@@ -0,0 +1,5 @@
+from PyInstaller.utils.hooks import copy_metadata, collect_data_files
+datas = copy_metadata("streamlit")
+datas += collect_data_files("streamlit")
+# Sometimes helpful on older PyInstaller:
+hiddenimports = ["importlib.metadata"]
